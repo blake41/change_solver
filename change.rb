5c576dde88a0769ba175224b9ec9037082052244
Blake Johnson <blake41@gmail.com>
2014-01-23 14:25:01 -0500

    sort of working

---
+++ change.rb
@@ -0,0 +1,29 @@
+require 'debugger'
+class ChangeMaker
+    
+  def initialize(coins = [50,25])
+    @coins = coins.sort
+  end
+
+  def possible(amount)
+    @coins.select do |coin|
+      coin <= amount
+    end
+  end
+
+  def make_change(amount)
+    return {0 => []} if amount == 0
+    change = Hash.new {|h,k| h[k] = []}
+    possible(amount).each do |coin|
+      change[amount] << ([coin] + make_change(amount - coin)[amount - coin])
+    end
+    return change
+  end
+
+end
+
+change = ChangeMaker.new.make_change(100)
+
+debugger
+
+puts 'hi'
