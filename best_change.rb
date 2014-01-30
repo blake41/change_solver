bc1973e6bed87df06468bb965ba2d1163fff5a00
Blake Johnson <blake41@gmail.com>
2014-01-30 13:17:11 -0500

    new solution where i pass state along

--- best_change.rb
+++ best_change.rb
@@ -1,6 +1,23 @@
+require 'debugger'
+class Array
+
+  def my_collect
+    to_return = []
+    if self.length == 1
+      to_return = yield self.first
+    elsif self.length > 1
+      to_return = self.collect do |element|
+        yield element
+      end
+    end
+    return to_return
+  end
+
+end
+
 class ChangeMaker
     
-  def initialize(coins = [20,10,5,2,1])
+  def initialize(coins = [20,10,5,1])
     @coins = coins.sort
     @cache = Hash.new {|h,k| h[k] = []}
   end
@@ -11,26 +28,48 @@ class ChangeMaker
     end
   end
 
-  def make_change(amount)    
-    return {amount => @cache[amount]} if @cache[amount].size > 0
-    return {0 => []} if amount == 0
-    change = Hash.new {|h,k| h[k] = []}
-    possible(amount).each do |coin|
-      solutions = make_change(amount - coin)[amount - coin]
-      if solutions.size > 0
-        solutions.each do |solution|
-          change[amount] << [coin] + solution
-        end
-      else
-        change[amount] << [coin]
+  def run(amount)
+    num_coins(make_change(amount, []))
+  end
+
+  def num_coins(solutions)
+    answer = solutions.sort {|solutiona, solutionb| solutiona.size <=> solutionb.size }.collect do |solution|
+      {solution.size => solution}
+    end
+    remove_dups(answer)
+  end
+
+  def get_keys(solutions)
+    solutions.collect do |solution|
+      solution.keys.first
+    end.uniq
+  end
+
+  def solutions_with_key(key, solutions)
+    solutions.select{|solution| solution.keys.include?(key)}.collect do |solution|
+      solution[key]
+    end
+  end
+
+  def remove_dups(solutions)
+    array = []
+    get_keys(solutions).each do |key|
+      solutions_with_key(key, solutions).collect {|subarray| subarray.sort }.uniq.each do |solution|
+        array << {key => solution}
       end
     end
-    temp = [change[amount].sort {|solutiona, solutionb| solutiona.size <=> solutionb.size}.first]
-    @cache[amount] = temp
-    change[amount] = temp
-    return change
+    return array
+  end
+
+  def make_change(amount, change)
+    return [change] if amount == 0
+    return possible(amount).collect do |coin|
+      make_change(amount - coin, change + [coin])
+    end.flatten(1)
   end
 
 end
-
-puts ChangeMaker.new.make_change(301).inspect
+t1 = Time.now
+puts ChangeMaker.new.run(25).inspect
+t2 = Time.now
+puts "it took #{t2 - t1} seconds to solve this"
